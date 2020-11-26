01189f23b908c67b813acc3576c4659b2b37570d
Luiz Amaral <alaluizamaral@gmail.com>
2020-11-26 08:20:26 -0300

    Fim secao 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,14 @@ import PostItem from '../components/PostItem'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="30 de Julho de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />
   </Layout>
 )
 
