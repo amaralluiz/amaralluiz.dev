ea74cf5e53f497aa7e9a9834e48e7a8ae9588a7e
Luiz Amaral <alaluizamaral@gmail.com>
2020-11-26 20:32:43 -0300

    fim da sessao 7

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,29 +23,55 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 }
 
 exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
+  const { createPage } = actions
 
-    return graphql(`
-     {
-        allMarkdownRemark {
-          edges {
-            node {
-              fields {
-                slug
-              }
-            }
+  return graphql(`
+  {
+    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      edges {
+        node {
+          fields {
+            slug
           }
+          frontmatter {
+            background
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            title
+          }
+          timeToRead
         }
       }
-    `).then(result => {
-        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-            createPage({
-                path: node.fields.slug,
-                component: path.resolve('./src/templates/blog-post.js'),
-                context: {
-                    slug: node.fields.slug
-                }
-            })
+    }
+  }
+  `).then(result => {
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
+        createPage({
+            path: node.fields.slug,
+            component: path.resolve('./src/templates/blog-post.js'),
+            context: {
+                slug: node.fields.slug
+            }
         })
     })
+
+    const postsPerPage = 6
+    const numPages = Math.ceil(posts.length / postsPerPage)
+
+    Array.from({ length: numPages }).forEach((_, index) =>{
+      createPage({
+        path: index === 0 ? '/' : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1
+        }
+      })
+    })
+  })
 }
