02204c35fc315864049d3c84ec0bab2ab99225c0
Luiz Amaral <alaluizamaral@gmail.com>
2020-12-18 17:47:30 -0300

    Fim da sessao 12

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     position: `Desenvolvedor Web`,
     description: `Blog feito como meu diário de estudo, para compartilhar o que venho aprendendo.`,
     author: `Luiz Amaral`,
+    siteUrl: `https://amaralluiz.dev`
   },
   plugins: [
     `gatsby-plugin-transition-link`,
@@ -72,17 +73,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Luiz Amaral`,
+        short_name: `Luiz Amaral`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -15,7 +15,7 @@ const BlogPost = ({ data, pageContext }) => {
 
     return (
         <Layout>
-          <SEO title={post.frontmatter.title}/>
+          <SEO title={post.frontmatter.title} description={post.frontmatter.descript}/>
           <S.PostHeader>
             <S.PostDate>
               {post.frontmatter.date} • {post.timeToRead} min de leitura
