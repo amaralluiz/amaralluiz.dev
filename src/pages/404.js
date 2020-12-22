f146190ddf4106fb3ddbad50daa6402974b73fe6
Luiz Amaral <alaluizamaral@gmail.com>
2020-12-22 09:06:32 -0300

    Atualizacao pagina 404 e about

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,15 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
+import { MainContent } from '../components/Post/styled'
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404</h1>
-    <p>Ops... Não tem nada aqui</p>
+    <MainContent>
+      <h1>404</h1>
+      <p>Ops... Não tem nada aqui</p>
+    </MainContent>
   </Layout>
 )
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -17,23 +17,20 @@ const AboutPage = () => (
       <p>
         Meu nome é Luiz Filipe Amaral, sou natural de Itaúna/MG.
       </p>
-
       <p>
-        Já passei trabalhei em empresas de diversas áreas, mas geralmente no setor de TI.
+        Já trabalhei em empresas de diversas áreas, mas geralmente no setor de TI.
         Sou formado em Análise e Desenvolvimento de Sistemas pelo Centro Universitário Estácio
         de Belo Horizonte. Antes disso comecei a estudar Ciência da Computação em Itaúna, mas
-        tive alguns problemas e tive que trancar o curso. Sempre gostei bastante da área de tecnologia
+        tive alguns problemas e acabei trancando o curso. Sempre gostei bastante da área de tecnologia
         e gostei muito de desenvolvimento, que acabou sendo a área que escolhi atuar e estudar
-        hoje em dia.
+        hoje em dia. Pretendo fazer outra faculdade, mas ainda estou analisando a ideia.
       </p>
-
       <p>
         Amo aprender e ensinar o que aprendi, tenho até alguma facilidade para isso, o que me deu a
-        ideia de criar o blog.
+        ideia de criar o blog. Pretendo postar aqui coisas interessantes que venho aprendendo sobre
+        desenvolvimento e sobre a área de TI.
       </p>
-
       <h2>Contato</h2>
-
       <p>
         Para entrar em contato comigo bastar enviar um email para <a href="mailto:contato@amaralluiz.dev">contato@amaralluiz.dev</a>
       </p>
